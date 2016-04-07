297800b5955e5998b0bd56ecc217ece9f946e36d
walters954 <wwalt010@fiu.edu>
2016-04-07 01:33:30 -0400

    Added the Js files

---
+++ public/js/main.js
@@ -0,0 +1,74 @@
+var nickname = 'guest', socket = io('http://localhost:8082'), uuid = localStorage.getItem('uuid'), store = {
+    users: new Array(),
+    count: 0,
+    channels: {}
+};
+var appendMessage = function (msg, channel) {
+    if ($('.tab-primary').html() === channel) {
+        $('.messages').append("\n    <div class=\"message\">\n      <div style=\"background-image:url(http://lorempixel.com/g/128/128/cats/" + (Math.floor(10 * Math.random()) + 1) + ")\" class=\"message-icon\"></div>\n      <div class=\"message-content\">\n        <p>" + msg + "</p>\n      </div>\n    </div>");
+    }
+    if (store.channels[channel])
+        store.channels[channel].messages.push(msg);
+};
+var appendTabs = function (msg) {
+    $('.tabs').append("\n        <li class=\"tab\">" + msg + "</li>\n    ");
+};
+$(function () {
+    socket.on('connect', function () {
+        if (!uuid) {
+            var randomid = Math.random().toString(36).substring(3, 16) + +new Date;
+            localStorage.setItem('uuid', randomid);
+            uuid = randomid;
+        }
+        socket.emit('register', uuid);
+        socket.emit('channelChange', $('.tab-primary').html());
+    });
+});
+socket.on('sync-store', function (s) {
+    store = JSON.parse(s);
+});
+socket.on('message', appendMessage);
+socket.on('nickname', function (msg) {
+    $('.messages').append("\n  <div class=\"message\">\n    <div style=\"background-image:url(http://lorempixel.com/g/128/128/cats/" + (Math.floor(10 * Math.random()) + 1) + ")\" class=\"message-icon\"></div>\n    <div class=\"message-content\">\n      <p>System: Changed Nickname to: " + msg + "</p>\n    </div>\n  </div>");
+});
+socket.on('clear', function () {
+    $('.messages').html("");
+});
+socket.on('leave', function () {
+    $('.tab').removeClass('tab-primary');
+    $("li:contains(#announcements)").addClass('tab-primary');
+});
+socket.on('join', function (t) {
+    $('.tab').removeClass('tab-primary');
+    console.log(t);
+    $("li:contains(" + t + ")").addClass('tab-primary');
+});
+socket.on('admin', function () {
+    $('.tabs').append("\n      <li class=\"tab\">#admin</li>\n  ");
+});
+socket.on('createTab', appendTabs);
+socket.on('delete-tab', function (t) {
+    console.log(t);
+    $('.tab').remove(":contains(" + t + ")");
+});
+$('#allTabs').on('click', '.tab', function () {
+    if (!$(this).hasClass('tab-primary')) {
+        $('.tab').removeClass('tab-primary');
+        $(this).addClass('tab-primary');
+        $('.messages').html("");
+        var channel = store.channels[$(this).html()];
+        socket.emit('channelChange', $('.tab-primary').html());
+        if (channel) {
+            channel.messages.map(function (m) {
+                $('.messages').append("\n        <div class=\"message\">\n          <div style=\"background-image:url(http://lorempixel.com/g/128/128/cats/" + (Math.floor(10 * Math.random()) + 1) + ")\" class=\"message-icon\"></div>\n          <div class=\"message-content\">\n            <p>" + m + "</p>\n          </div>\n        </div>");
+            });
+        }
+    }
+});
+$('form').submit(function () {
+    if (/\S/.test($('#message-input').val()))
+        socket.emit('message', $('#message-input').val(), $('.tab-primary').html());
+    $('#message-input').val('');
+    return false;
+});
+//# sourceMappingURL=main.js.map
---
+++ server.js
@@ -0,0 +1,88 @@
+"use strict";
+var Express = require('express');
+var Http = require('http');
+var Sockets = require('socket.io');
+var chat_session_1 = require('./chat-session');
+var chat_commands_1 = require('./chat-commands');
+var app = Express();
+var http = Http.createServer(app);
+var io = Sockets(http);
+var commands = chat_commands_1.default(io, chat_session_1.default);
+app.use(Express.static(__dirname + '/public'));
+app.get('/chatapp', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+io.on('connection', function (socket) {
+    var player;
+    socket.on('sync-store', function () {
+        socket.emit('sync-store', JSON.stringify(chat_session_1.default));
+    });
+    socket.on('register', function (uuid) {
+        socket.emit('sync-store', JSON.stringify(chat_session_1.default));
+        if (!(player = chat_session_1.default.users[uuid])) {
+            chat_session_1.default.count++;
+            player = chat_session_1.default.users[uuid] = {
+                uuid: uuid,
+                tabs: 0,
+                nick: 'guest' + chat_session_1.default.count,
+                socket: socket,
+                type: 'user',
+                currentChat: '#soccer',
+                quit: false
+            };
+            io.sockets.emit('nickname', player.nick);
+            if (player.nick == 'guest1') {
+                player.type = 'sysop';
+            }
+        }
+        else {
+            player = chat_session_1.default.users[uuid];
+            socket.emit('log', chat_session_1.default.channels['#anouncements'].messages);
+            socket.emit('message', 'System: Welcome back ' + player.nick + '!', '#anouncements');
+            if (!player.disconnected) {
+                player.tabs++;
+                socket.disconnect();
+            }
+        }
+        if (player.disconnected) {
+            clearTimeout(player.timeout);
+            player.disconnected = false;
+        }
+        chat_session_1.default.users[uuid] = player;
+    });
+    socket.on('disconnect', function (type) {
+        if (type == 'booted' && player.tabs > 0)
+            return;
+        player.disconnected = true;
+        player.timeout = setTimeout(function () {
+            if (player.disconnected) {
+                delete chat_session_1.default.users[player.uuid];
+                chat_session_1.default.count--;
+            }
+        }, 2000);
+    });
+    socket.on('message', function (msg, channel) {
+        if (!commands.isCommand(msg) && !player.quit) {
+            var out = player.nick + ': ' + msg;
+            io.emit('message', out, channel);
+            var curChannel = chat_session_1.default.channels[channel];
+            if (curChannel)
+                curChannel.messages.push(out);
+            console.log(chat_session_1.default.channels[channel]);
+        }
+        else if (commands.isCommand(msg)) {
+            commands.run(player, msg);
+        }
+        else {
+            return;
+        }
+    });
+    socket.on('channelChange', function (channel) {
+        player.currentChat = channel;
+        console.log(player.nick + ' moved to channe: ' + player.currentChat);
+    });
+});
+var port = 8082;
+http.listen(port);
+console.log('Chat App Online @ localhost:' + port);
+//# sourceMappingURL=server.js.map
